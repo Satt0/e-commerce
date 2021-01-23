de53d07bdc687f3df4bca2833b55a69d0dae9d17
tan <minhtanbg6601@gmail.com>
2021-01-23 23:33:30 +0700

    ok

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {makeStyles} from '@material-ui/core'
 import loading from './loading.svg'
+import anime from 'animejs/lib/anime.es.js';
+
 const useStyles=makeStyles((theme)=>({
     root:{
         width:'100%',
         height:'100%',
         display:'flex',
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+        overflow:'hidden'
+    },
+    loading:{
+        position:'absolute',
+        borderRadius:'100%',
+        
+        userSelect:'none'
     }
 }))
 export default function Loading() {
     const style=useStyles()
+
+
+   
+    
     return (
         <div className={style.root}>
-            <img src={loading} alt="loading icon"></img>
+            <img className={style.loading} src={loading} alt="loading icon"></img>
+           
+           
         </div>
     )
 }
